39c532ed94da838284a6f9da02183adbec3efbe9
Vinicius Freire de Freitas <Vinicius.Freitas@anchieta.br>
2023-05-16 15:14:37 -0300

    config: setup backend project

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+
+const app = fastify();
+const port = 3333;
+
+app.get("/hello", () => {
+  return "Hello World";
+});
+
+app
+  .listen({
+    port,
+  })
+  .then(() => {
+    console.log(`🚀 HTTP server running on port ${3333}`);
+  });
