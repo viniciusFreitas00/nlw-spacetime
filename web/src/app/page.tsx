7af1cae27f997cc791a649c9c9e98b2c47894dfe
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-05-18 17:56:36 -0300

    feat: create first component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Button from "@/components/Button";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1>
+      Hello World
+      <Button title="teste" />
+      <Button title="teste 1" />
+      <Button title="teste 2" />
+      <Button title="teste 3" />
+    </h1>
+  );
 }
---
+++ web/src/components/Button.tsx
@@ -0,0 +1,7 @@
+type ButtonProps = {
+  title: string
+}
+
+export default function Button ({title}: ButtonProps) {
+  return <p>{title}</p>
+}
