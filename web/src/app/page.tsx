37e9891528aa8c064ab2af97dc12f207f7e36e00
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-05-19 17:18:19 -0300

    feat: inital layout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="m-8 h-16 w-16 bg-red-800 text-2xl font-bold">oi</div>;
+  return (
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  );
 }
