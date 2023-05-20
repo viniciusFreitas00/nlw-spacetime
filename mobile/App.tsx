9dd2fd72a4c5390a471026b737f9f4df157297b7
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-05-19 22:45:06 -0300

    config: add prettier and esling to mobile project

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl text-center ">
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-center text-5xl font-bold text-zinc-50 ">
         Open up to start working on your app!
       </Text>
       <StatusBar style="light" />
     </View>
-  );
+  )
 }
