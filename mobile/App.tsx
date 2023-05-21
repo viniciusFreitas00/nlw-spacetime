76f82c95a54930721169d1b5f08dd67628b9eddc
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-05-21 19:05:07 -0300

    config: add font to mobile project

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-center text-5xl font-bold text-zinc-50 ">
-        Open up to start working on your app!
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-center font-title text-5xl text-gray-50">
+        Rocketseat
       </Text>
       <StatusBar style="light" />
     </View>
