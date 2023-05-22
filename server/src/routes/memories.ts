ee1227e420efd68cec651ff661c79cbd2a997b08
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-05-22 14:51:56 -0300

    feat: create post memory route

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-
+import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
@@ -10,12 +10,45 @@ export async function memoriesRoutes(app: FastifyInstance) {
       },
     })
 
-    return memories
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        converUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat(' ...'),
+      }
+    })
   })
 
-  app.get('/memories/:id', async () => {})
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({ id: z.string().uuid() })
 
-  app.post('/memories', async () => {})
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({ where: { id } })
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+      coverUrl: z.string(),
+    })
+
+    const { content, isPublic, coverUrl } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '6d6de64d-d73a-4048-b5b7-755be5e2541e',
+      },
+    })
+
+    return memory
+  })
 
   app.put('/memories/:id', async () => {})
 
