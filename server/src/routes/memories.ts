6b4912166a5669db8a9aa6a5f99e45fb01589832
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-05-22 14:53:56 -0300

    feat: create delete memory route

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -52,5 +52,11 @@ export async function memoriesRoutes(app: FastifyInstance) {
 
   app.put('/memories/:id', async () => {})
 
-  app.delete('/memories/:id', async () => {})
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({ id: z.string().uuid() })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({ where: { id } })
+  })
 }
