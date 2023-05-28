ceb7e5600d93e9eec63f53e69f8910b6699dc8a6
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-05-28 19:01:23 -0300

    feat: display user profile

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import { api } from '@/lib/api'
-import { request } from 'http'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
