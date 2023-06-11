881724a0a35ba24f9a856183f193da6c90e43a1c
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-06-11 10:30:06 -0300

    feat: add middleware to check if the user is logged

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -5,6 +5,7 @@ import { api } from '@/lib/api'
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
+  const redirectTo = request.cookies.get('redirectTo')?.value
 
   const resgisterResponse = await api.post('/register', {
     code,
@@ -12,7 +13,7 @@ export async function GET(request: NextRequest) {
 
   const { token } = resgisterResponse.data
 
-  const redirecURL = new URL('/', request.url)
+  const redirecURL = redirectTo ?? new URL('/', request.url)
 
   const cookieExpiresInSecondes = 60 * 60 * 24 * 60
 
